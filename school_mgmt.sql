93d02ce902c5df3be689d9760defcdc8eef13a7b
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2023-03-10 17:23:19 +0530

    Updated Database

--- school_mgmt.sql
+++ school_mgmt.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 06, 2023 at 12:58 AM
--- Server version: 8.0.32-0ubuntu0.22.04.2
--- PHP Version: 7.4.33
+-- Host: localhost:3306
+-- Generation Time: Mar 10, 2023 at 05:22 PM
+-- Server version: 8.0.32-0ubuntu0.20.04.2
+-- PHP Version: 7.4.3-4ubuntu2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -40,13 +41,13 @@ CREATE TABLE `institute_registration` (
   `institute_address` varchar(550) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `admin_profile_pic` varchar(350) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `community_status` varchar(350) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `account_status` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
-  `account_type` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
-  `supp` varchar(350) COLLATE utf8mb4_general_ci NOT NULL,
-  `transaction_id` varchar(350) COLLATE utf8mb4_general_ci NOT NULL,
-  `renewal_date` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
-  `validity_upto` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
-  `takeover_date` varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
+  `account_status` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `account_type` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `supp` varchar(350) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `transaction_id` varchar(350) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `renewal_date` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `validity_upto` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `takeover_date` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `portal_registration_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -55,7 +56,24 @@ CREATE TABLE `institute_registration` (
 --
 
 INSERT INTO `institute_registration` (`id`, `institute_name`, `admin_name`, `admin_role`, `hod_contact`, `login_id`, `password`, `inst_reg_id`, `institute_email`, `institute_address`, `admin_profile_pic`, `community_status`, `account_status`, `account_type`, `supp`, `transaction_id`, `renewal_date`, `validity_upto`, `takeover_date`, `portal_registration_date`) VALUES
-(1, 'Green Vila', 'Sinha', 'super_admin', '+91-8520147963', 'GREE03237963', 'Admin1234#@', 'Enter Institute Registration Details', 'email@yourinstitute.com', 'Your Institute Address Here', 'http://localhost/school_mgmt_system/modules/avatar/sample/avatar2.png', 'Not Published', 'Running', 'Trial Account', 'To be updated', 'Needs Update', 'Needs Update', 'Needs Update', 'New Principal Joining Date', '2023-03-05 21:57:12');
+(1, 'Aadamya', 'Vivek Robin Kujur', 'super_admin', '+91-9264453821', 'AADA03233821', 'Admin1234#@', 'Enter Institute Registration Details', 'email@yourinstitute.com', 'Your Institute Address Here', 'http://localhost/school_mgmt_system/modules/avatar/sample/avatar13.png', 'Not Published', 'Account_Suspended', 'Trial Account', 'To be updated', 'Needs Update', 'Needs Update', 'Needs Update', 'New Principal Joining Date', '2023-03-10 12:30:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `webmaster_portal`
+--
+
+CREATE TABLE `webmaster_portal` (
+  `id` int NOT NULL,
+  `webmaster_name` varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
+  `webmaster_contact` varchar(12) COLLATE utf8mb4_general_ci NOT NULL,
+  `webmaster_email` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  `webmaster_image` varchar(350) COLLATE utf8mb4_general_ci NOT NULL,
+  `webmaster_password` varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
+  `webmaster_referal_ID` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `portal_regDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -68,6 +86,12 @@ ALTER TABLE `institute_registration`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `webmaster_portal`
+--
+ALTER TABLE `webmaster_portal`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -76,6 +100,12 @@ ALTER TABLE `institute_registration`
 --
 ALTER TABLE `institute_registration`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `webmaster_portal`
+--
+ALTER TABLE `webmaster_portal`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
